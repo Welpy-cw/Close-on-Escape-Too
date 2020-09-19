91398f9ff2875e37e02396f4e6a51f947c348200
Welpy-cw <h.w.forum@arcor.de>
2020-09-19 20:19:38 +0200

    restored (slightly limited) functionality

--- chrome/content/overlay-messenger.js
+++ chrome/content/overlay-messenger.js
@@ -1,44 +1,51 @@
 var closetabonesc = {
   prefs: null,
-  
+  alreadyLoaded: false,
+
   startup: function(e) {
     closetabonesc.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.CloseTabOnEsc@david-winter.at.");
-    window.addEventListener("keydown", closetabonesc.onKeyPress, false);
+    if (!this.alreadyLoaded) {
+      this.alreadyLoaded = true;
+      window.addEventListener("keydown", closetabonesc.onKeyPress, false);
+    }
   },
-  
-  onKeyPress: function(e) { 
+
+  onKeyPress: function(e) {
     if (e.key == "Escape") {
       var tabmail = document.getElementById('tabmail');
-      if (tabmail.tabInfo.length > 1) {     
+      if (tabmail.tabInfo.length > 1) {
         var idx = tabmail.tabContainer.selectedIndex;
         if (closetabonesc.prefs.getBoolPref("onlymessagetabs") && tabmail.tabInfo[idx].mode.name != 'message') {
           return;
         }
-        
-        if (closetabonesc.prefs.getBoolPref("gotofirsttab")) {  
+        if (closetabonesc.prefs.getBoolPref("gotofirsttab")) {
           tabmail.selectTabByIndex(null, 0);
+        } else {
+          tabmail.selectTabByIndex(null, tabmail.tabInfo.length > idx ? idx : (tabmail.tabInfo.length >= 1 ? tabmail.tabInfo.length - 1 : 0));
         }
-		else {
-		  tabmail.selectTabByIndex(null, tabmail.tabInfo.length > idx ? idx : (tabmail.tabInfo.length >= 1 ? tabmail.tabInfo.length - 1 : 0));
-		}
-        tabmail.removeTabByNode(tabmail.tabContainer.childNodes[idx]);
-		e.preventDefault();
+        tabmail.closeTab(idx);
+        e.preventDefault();
+      }
+    }
+  },
+
+  beforeUnload: function(e) {
+    e.preventDefault();
+    // Refresh preferences
+    closetabonesc.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.CloseTabOnEsc@david-winter.at.");
+
+    if (closetabonesc.prefs.getBoolPref("closetabsonexit")) {
+      var tabmail = document.getElementById('tabmail');
+      for (var idx = tabmail.tabInfo.length - 1; idx >= 0; idx--) {
+        if (tabmail.tabInfo[idx].mode.name == 'message') {
+          tabmail.closeTab(idx);
+        }
       }
     }
   },
   
-  beforeUnload: function(e) {
-	// Refresh preferences
-	closetabonesc.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.CloseTabOnEsc@david-winter.at.");
-	
-    if (closetabonesc.prefs.getBoolPref("closetabsonexit")) {
-      var tabmail = document.getElementById('tabmail');
-      for (var idx = tabmail.tabInfo.length-1; idx >= 0; idx--) {
-        if (tabmail.tabInfo[idx].mode.name == 'message') {
-          tabmail.removeTabByNode(tabmail.tabContainer.childNodes[idx]);
-        }
-      }
-    }
+  cleanup: function() {
+    window.removeEventListener("keydown", closetabonesc.onKeyPress, false);
   }
 };
 
@@ -47,8 +54,5 @@ function onLoad(activatedWhileWindowOpen) {
 }
 
 function onUnload(deactivatedWhileWindowOpen) {
-  if (!deactivatedWhileWindowOpen) {
-    return;
-  }
-  closetabonesc.beforeUnload();
+  closetabonesc.cleanup();
 }
