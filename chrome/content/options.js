82a211ee273709164b43d1a65cb19459994bedd2
Welpy-cw <h.w.forum@arcor.de>
2020-09-19 16:16:02 +0200

    should be working

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -1,7 +1,6 @@
-var { Services } = ChromeUtils.import('resource://gre/modules/Services.jsm');
-
-Services.console.logStringMessage("CoET: Loading options.xhtml preferences");
-console.log("CoET: Loading options.xhtml preferences");
+var {
+  Services
+} = ChromeUtils.import('resource://gre/modules/Services.jsm');
 
 Preferences.addAll([{
     id: "extensions.CloseTabOnEsc@david-winter.at.gotofirsttab",
